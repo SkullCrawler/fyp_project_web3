71fe4e3d16958758febdd082e4ab489e55d59942
SkullCrawler <69508787+SkullCrawler@users.noreply.github.com>
2024-04-27 15:08:07 +0500

    updated some configuration

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,3 @@ export { default as Welcome } from "./Welcome";
 export { default as Footer } from "./Footer";
 export { default as Services } from "./Services";
 export { default as Transactions } from "./Transactions";
-export { default as FlexBetweeen } from "./FlexBetween";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,14 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    goreli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/i6nL-ljOSqULHKFdfpf67pRYUjwRNFjp',
-      accounts: ['456adfcf31519f901610983b95d48b11e47985657014ec3a3d0cf94ff16ce176'],
-    },
-    bsctestnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545', // Binance Smart Chain Testnet URL
-      chainId: 97, // Chain ID for BSC Testnet
-      accounts: ['456adfcf31519f901610983b95d48b11e47985657014ec3a3d0cf94ff16ce176'], // Replace with your BSC Testnet mnemonic private key
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
     },
   },
 };
