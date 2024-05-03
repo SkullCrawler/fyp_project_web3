cfd278b452d4b6329d067716cd43580b52024e78
SkullCrawler <69508787+SkullCrawler@users.noreply.github.com>
2024-05-03 23:15:18 +0500

    changes to hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,11 +5,7 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
-      chainId: 31337,
-      accounts: ['
-      0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e',
-      '0x7019d3Cc0a363c30d0D31bF55cC6351c78f09A50'
-    ],
+      chainId: 31337
     },
   },
 };
