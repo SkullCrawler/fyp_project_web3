ea2fb9e2adf42cebdd57e832dfe8586201f23c3d
Rayyan Waseem <69508787+SkullCrawler@users.noreply.github.com>
2024-07-12 16:54:16 +0500

    production changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
+// import { HiMenuAlt4 } from "react-icons/hi";
+// import { AiOutlineClose } from "react-icons/ai";
 import logo from "../../images/logo.png";
 
-const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
-);
+// const NavBarItem = ({ title, classprops }) => (
+//   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+// );
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      {/* <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
@@ -54,7 +54,7 @@ const Navbar = () => {
             )}
           </ul>
         )}
-      </div>
+      </div> */}
     </nav>
   );
 };
